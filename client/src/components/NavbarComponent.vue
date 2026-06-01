4f4253363be3d24ac598205f497339b4fd4eda3a
mpbd525 <mpbd525@gmail.com>
2026-06-02 06:19:17 +0800

    CORS

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container">
 
-      <router-link :to="{ name: 'Home'}" class="navbar-brand">Meeple Tavern</router-link>
+      <router-link :to="{ name: 'Home'}" class="navbar-brand">CatFruit</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
