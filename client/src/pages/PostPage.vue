971a5f2524330722e2ef9da7a9d7ed587ccb11dc
mpbd525 <mpbd525@gmail.com>
2026-06-03 05:29:30 +0800

    PostPage bugfix

--- client/src/pages/PostPage.vue
+++ client/src/pages/PostPage.vue
@@ -1,15 +1,24 @@
 <script setup>
+	import { ref, computed, onMounted } from 'vue';
 	import { useRoute } from 'vue-router';
-	import { computed } from 'vue';
+	import { storeToRefs } from 'pinia';
+	import { useGlobalStore } from '../stores/global';
+	import { Notyf } from 'notyf';
+	import api from '../api';
 
 	const route = useRoute();
 	const postId = route.params.postId;
+	const notyf = new Notyf();
+
+	const store = useGlobalStore();
+	const { user } = storeToRefs(store);
 
 	const post = ref(null);
 
 	onMounted(async() => {
 		try{
 			const { data } = await api.get(`/posts/${postId}`);
+			post.value = data.result;
 		} catch(e) {
 			notyf.error("Post not found");
 		}
@@ -21,12 +30,14 @@
 </script>
 
 <template>
-	<div v-if="post">
-		<h1>{{ post.title }}</h1>
-		<p class="text-muted">{{ new Date(post.creation_date).toLocalString() }}</p>
-		<p>{{ post.content }}</p>
-		<router-link v-if="canEdit" :to="{ name: 'EditPost', params: { postId: post._id }}" class="btn btn-primary">
-			Edit
-		</router-link>
+	<div v-if="post" class="container-fluid">
+		<div class="col-md-8 mx-auto my-5">
+			<h1>{{ post.title }}</h1>
+			<p class="text-muted small">{{ new Date(post.creation_date).toLocalString() }}</p>
+			<p>{{ post.content }}</p>
+			<router-link v-if="canEdit" :to="{ name: 'EditPost', params: { postId: post._id }}" class="btn btn-primary">
+				Edit
+			</router-link>
+		</div>
 	</div>
 </template>
